cb643b6c0f9f762ca551bc2bbe12dd304ccdde66
orhadar1 <or.hadar01@post.runi.ac.il>
2025-11-05 14:02:34 +0200

    end?

--- CalcPi.java
+++ CalcPi.java
@@ -14,6 +14,6 @@ public class CalcPi {
             }
         }
         System.out.println("pi according to Java: " + Math.PI);
-        System.out.println("pi, approximated:     " + String.valueOf(sum * 4));
+        System.out.println("pi, approximated:     " + sum * 4);
     }
 }
