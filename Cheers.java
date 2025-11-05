6b87d4ba63afa8e76884f81556ca381697194849
orhadar1 <or.hadar01@post.runi.ac.il>
2025-11-05 02:18:49 +0200

    finish?

--- Cheers.java
+++ Cheers.java
@@ -1,23 +1,19 @@
 public class Cheers {
         
-    public static void main(String[] args) {
-        String word = args[0].toUpperCase();         
+    public static void main(String[] args) { 
+        String word = args[0].toUpperCase();
         int times = Integer.parseInt(args[1]);
 
-        
-        final String IsAn = "AEFHILMNORSX";
+        final String AN_LETTERS = "AEFHILMNORSX";
 
-       
-        for (char ch : word.toCharArray()) 
-        {
-            String aoran = (IsAn.indexOf(ch) >= 0) ? "an" : "a"; 
-            System.out.println("Give me " + aoran + " " + ch + ": " + ch + "!");
+        for (char ch : word.toCharArray()) {
+            boolean needsAn = AN_LETTERS.indexOf(ch) >= 0;
+            String prefix = needsAn ? "an " : "a  ";
+            System.out.println("Give me " + prefix + ch + ": " + ch + "!");
         }
 
-        
         System.out.println("What does that spell?");
 
-        
         String cheer = word + "!!!";
         for (int i = 0; i < times; i++) {
             System.out.println(cheer);
