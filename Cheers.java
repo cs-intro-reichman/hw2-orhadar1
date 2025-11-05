b41c491e1d9a27a1e9c207fed2fef4ba1ca782a4
orhadar1 <or.hadar01@post.runi.ac.il>
2025-11-05 02:15:40 +0200

    myb finish

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,26 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+        
+    public static void main(String[] args) {
+        String word = args[0].toUpperCase();         
+        int times = Integer.parseInt(args[1]);
+
+        
+        final String IsAn = "AEFHILMNORSX";
+
+       
+        for (char ch : word.toCharArray()) 
+        {
+            String aoran = (IsAn.indexOf(ch) >= 0) ? "an" : "a"; 
+            System.out.println("Give me " + aoran + " " + ch + ": " + ch + "!");
         }
+
+        
+        System.out.println("What does that spell?");
+
+        
+        String cheer = word + "!!!";
+        for (int i = 0; i < times; i++) {
+            System.out.println(cheer);
+        }
+    }
 }
